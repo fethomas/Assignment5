d855c0133d60144f4852583cf7fe7cc337137d6f
fethomas <fethomas@ursinus.edu>
2018-11-05 16:42:29 -0500

    updated Shakespeare

--- train.sh
+++ train.sh
@@ -3,12 +3,12 @@ MODEL_DIR=models
 rm -Rf $MODEL_DIR
 mkdir -p $MODEL_DIR
 python3 -m sockeye.train\
-                       --source $DATA_DIR/shakespeare/sparknotes/merged/antony-and-cleopatra_modern.snt.aligned \
-                       --target $DATA_DIR/shakespeare/sparknotes/merged/antony-and-cleopatra_original.snt.aligned \
+                       --source $DATA_DIR/shakespeare/sparknotes/merged/modernTraining.snt.aligned \
+                       --target $DATA_DIR/shakespeare/sparknotes/merged/originalTraining.snt.aligned \
                        --encoder cnn \
                        --decoder cnn \
-                       --rnn-num-hidden 2 \
-                       --validation-source $DATA_DIR/shakespeare/sparknotes/merged/antony-and-cleopatra_modern.snt.aligned \
-                       --validation-target $DATA_DIR/shakespeare/sparknotes/merged/antony-and-cleopatra_original.snt.aligned \
+                       --rnn-num-hidden 512 \
+                       --validation-source $DATA_DIR/shakespeare/sparknotes/merged/modernValidation.snt.aligned \
+                       --validation-target $DATA_DIR/shakespeare/sparknotes/merged/originalValidation.snt.aligned \
                        --output $MODEL_DIR \
-                       --decode-and-evaluate 500
+                       --decode-and-evaluate 500   	

5cd49db3d0f363dd3b84f5e21966b0b5b682001b
fethomas <fethomas@ursinus.edu>
2018-11-07 16:36:46 -0500

    updated train

--- train.sh
+++ train.sh
@@ -2,18 +2,16 @@ DATA_DIR=tensorflow-shakespeare-master/tensorflow-shakespeare-master/data
 MODEL_DIR=models
 rm -Rf $MODEL_DIR
 mkdir -p $MODEL_DIR
-python3 -m sockeye.train\
-                       --source $DATA_DIR/shakespeare/sparknotes/merged/modernTraining.snt.aligned \
-                       --target $DATA_DIR/shakespeare/sparknotes/merged/originalTraining.snt.aligned \
-                       --encoder cnn \
-                       --decoder cnn \
-					   --cnn-activation-type relu \
-					   --optimizer adam \
-					   --rnn-attention-type bilinear \
-                       --rnn-num-hidden 512 \
-					   --max-num-epochs 300 \ 
-                       --validation-source $DATA_DIR/shakespeare/sparknotes/merged/modernValidation.snt.aligned \
-                       --validation-target $DATA_DIR/shakespeare/sparknotes/merged/originalValidation.snt.aligned \
-                       --output $MODEL_DIR \
-                       --decode-and-evaluate 500 \ 
-					   ##--initial-learning-rate .0001 
+python3 -m sockeye.train --source $DATA_DIR/shakespeare/sparknotes/merged/modernTraining.snt.aligned \
+        --target $DATA_DIR/shakespeare/sparknotes/merged/originalTraining.snt.aligned \
+        --encoder cnn \
+        --decoder cnn \
+	--cnn-activation-type relu \
+	--optimizer adam \
+	--rnn-attention-type bilinear \
+        --rnn-num-hidden 512 \
+	--max-num-epochs 300 \
+        --validation-source $DATA_DIR/shakespeare/sparknotes/merged/modernValidation.snt.aligned\
+	--validation-target $DATA_DIR/shakespeare/sparknotes/merged/originalValidation.snt.aligned \
+        --output $MODEL_DIR \
+        --decode-and-evaluate 500 \

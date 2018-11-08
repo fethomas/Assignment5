90d7e1e7b9b49e0a3184bd4941c1f95ab8015139
fethomas <fethomas@ursinus.edu>
2018-11-07 21:42:41 -0500

    without network

--- train.sh
+++ train.sh
@@ -9,8 +9,8 @@ python3 -m sockeye.train --source $DATA_DIR/shakespeare/sparknotes/merged/modern
 	#--cnn-activation-type relu \
 	#--optimizer adam \
 	#--rnn-attention-type bilinear \
-    --rnn-num-hidden 512 \
-	--max-num-epochs 300 \
+    #--rnn-num-hidden 512 \
+		--max-num-epochs 300 \
         --validation-source $DATA_DIR/shakespeare/sparknotes/merged/modernValidation.snt.aligned\
 	--validation-target $DATA_DIR/shakespeare/sparknotes/merged/originalValidation.snt.aligned \
         --output $MODEL_DIR \

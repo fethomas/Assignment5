cdfdf839d0e9f835f6312218743c2b885dc50b20
fethomas <fethomas@ursinus.edu>
2018-11-07 21:35:48 -0500

    new update

--- train.sh
+++ train.sh
@@ -6,9 +6,9 @@ python3 -m sockeye.train --source $DATA_DIR/shakespeare/sparknotes/merged/modern
         --target $DATA_DIR/shakespeare/sparknotes/merged/originalTraining.snt.aligned \
         --encoder cnn \
         --decoder cnn \
-	--cnn-activation-type relu \
-	--optimizer adam \
-	--rnn-attention-type bilinear \
+	#--cnn-activation-type relu \
+	#--optimizer adam \
+	#--rnn-attention-type bilinear \
         --rnn-num-hidden 512 \
 	--max-num-epochs 300 \
         --validation-source $DATA_DIR/shakespeare/sparknotes/merged/modernValidation.snt.aligned\
